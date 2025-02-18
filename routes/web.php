b63f71d2f3e2fa6e746f5c50bbcb579bbfef75d2
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-02-18 11:14:05 +0700

    J1 praktikum 1 - route groups dan route prefixes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -42,18 +45,46 @@ Route::get('/articles/{id}', function ($id) {
     return "Halaman artikel dengan ID {$id}";
 });
 
-Route::get('/user/{name?}', function ($name = "John") {
-    return 'Nama saya ' . $name;
-});
+// Route::get('/user/{name?}', function ($name = "John") {
+//     return 'Nama saya ' . $name;
+// });
 
-// Route::get('/user/profile', function () {
-//     return 'Ini adalah profile pengguna';
-// })->name('profile');
+Route::get('/user/profile', function () {
+    return 'Ini adalah profile pengguna';
+})->name('profile');
 
-// Route::get('/user/profile', [UserProfileController::class, 'show'])->name('profile');
+Route::get('/user/profile', [UserProfileController::class, 'show'])->name('profile');
 
 // // Generating URLs...
 // $url = route('profile');
 
 // // Generating Redirects...
 // return redirect()->route('profile');
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});

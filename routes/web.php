ce7858aa92b56dd3d980a3cbacb8d6b08578c54d
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-02-18 11:18:11 +0700

    feat: J1 praktikum 1 - redirect routes

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,5 @@ Route::prefix('admin')->group(function () {
     Route::get('/post', [PostController::class, 'index']);
     Route::get('/event', [EventController::class, 'index']);
 });
+
+Route::redirect('/here', '/there');

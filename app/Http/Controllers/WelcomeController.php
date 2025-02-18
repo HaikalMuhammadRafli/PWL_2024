0cc086ccf9ec07bda3519c2ecd6b798a6cdc1cf2
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-02-18 15:12:47 +0700

    feat: J1 praktikum 3 - Menampilkan View Dari Controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,10 @@ class WelcomeController extends Controller
 {
     public function hello()
     {
-        return 'Hello World';
+        return ('Hello World');
+    }
+    public function greeting()
+    {
+        return view('blog.hello', ['name' => 'Andi']);
     }
 }
